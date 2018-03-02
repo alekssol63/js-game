ff759d355965fda975a1742ccc81c962409ff802
alekssol63 <aleksvs83@yandex.ru>
2018-03-02 15:02:00 +0600

    Update game.js

--- game.js
+++ game.js
@@ -17,49 +17,42 @@ class Vector {
 }
 
 class Actor {
-  constructor(pos = new Vector(0, 0), size = new Vector(1, 1), speed = new Vector(0, 0)){
-    try {  
-	    if (!(pos instanceof Vector) ||!(size instanceof Vector) ||!(speed instanceof Vector)){
-		    throw new Error('Может быть передан только объект класса Vector')
-	    }
-	    this.pos = pos;
-      this.size = size;
-      this.speed = speed;
-      
-      Object.defineProperty(this, 'type', {value : 'actor', writable :false})	
-      this.act = function(){};
-	
-	  } catch(e) {
-		  console.log(e.message)
-	  }
-  } 	
-  get left(){
-    return this.pos.x
+  constructor(pos = new Vector(0, 0), size = new Vector(1, 1), speed = new Vector(0, 0)) {
+    if (!(pos instanceof Vector) ||!(size instanceof Vector) ||!(speed instanceof Vector)) {
+      throw new Error('Может быть передан только объект класса Vector')
+    }
+    this.pos = pos;
+    this.size = size;
+    this.speed = speed;
   }
-  get right(){
-   return this.pos.x + this.size.x;
+  act() {
+    return; 
   }
-  get bottom(){
+  get type() {
+    return 'actor';
+  }
+  get left() {
+    return this.pos.x;
+  }
+  get right() {
+    return this.pos.x + this.size.x;
+  }
+  get bottom() {
     return this.pos.y + this.size.y;
   }
-  get top(){
+  get top() {
     return this.pos.y;
   }
   isIntersect(actor) {
-    try {
-      if (!(actor instanceof  Actor)){
-        throw new Error('Неправильный аргумент')
-      }
-      if (actor === this) {
-        return false;
-      } else {
-	      if (this.left > actor.right || this.right < actor.left || this.top > actor.bottom || this.bottom < actor.top) {
-			    return false
-		    } else return true 
-      }   
-    
-    } catch(e) {
-      console.log(e.message)
+    if (!(actor instanceof  Actor)){
+      throw new Error('Неправильный аргумент');
     }
- }
+    if (actor === this) {
+      return false;
+    } else {
+      if (this.left > actor.right || this.right < actor.left || this.top > actor.bottom || this.bottom < actor.top) {
+        return false;
+      } else return true; 
+    }   
+  }
 }
